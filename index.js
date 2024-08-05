9eb6778d7ad9a4a56f456c8ea389368303a9b4a3
abhijit-hub <itsabhijitmore@gmail.com>
2024-08-05 14:59:37 +0530

    Demo

---
+++ index.js
@@ -0,0 +1,16 @@
+const emp={
+    name:"Abhiijt",
+    id:"0111",
+    salary:"10000",
+    isfine:this.id,
+    welcomeMessage: function() {
+        console.log(`${this.name} , welcome to website`);
+    }
+}
+
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
